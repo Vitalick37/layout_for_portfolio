267eede5fc31d3812d798f3b3d65cee405318f2f
Vitalik <vit198837@mail.ru>
2020-02-05 20:42:23 +0300

    поставил слайдер во 2 блоке

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        bulletClass: 	'swiper-pagination-bullet',
+        bulletActiveClass: 	'swiper-pagination-bullet-active',
+        clickable: true
+      },
+});
+
