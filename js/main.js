e06dd9bfec29af4c575b57c27d2e58856f687642
Vitalick37 <vit198837@mail.ru>
2020-02-12 20:04:44 +0300

    сделал навигацию

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+// fixed menu*******************************************************
+
+// let navPos, winPos, navHeight;
+  
+// function refreshVar() {
+//   navPos = $('.header-top').offset().top;
+//   navHeight = $('.header-top').outerHeight(true);
+// }
+
+// refreshVar();
+// $(window).resize(refreshVar);
+
+// $('<div class="clone-nav"></div>').insertBefore('.header-top').css('height', navHeight).hide();
+
+// $(window).scroll(function() {
+//   winPos = $(window).scrollTop();
+
+//   if (winPos >= navPos) {
+//     $('.header-top').addClass('fixed shadow');  
+//     $('.clone-nav').show();
+//   } 
+//   else {
+//     $('.header-top').removeClass('fixed shadow');
+//     $('.clone-nav').hide();
+//   }
+// });
+
+// slider*************************************************
+
 let mySwiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
@@ -8,3 +40,24 @@ let mySwiper = new Swiper('.swiper-container', {
       },
 });
 
+// mobile menu
+
+let menuButton = document.querySelector('.menu-button'),
+    menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('menu-button-active');
+    // menu.classList.toggle('header-active');
+});
+
+// скролл навигации
+
+let navLink = $('.menu-link');
+navLink.on('click', function(event) {
+event.preventDefault();
+let target = $(this).attr('href');
+let top = $(target).offset().top;
+$('html, body').animate({scrollTop: top}, 500)
+});
+
+});
